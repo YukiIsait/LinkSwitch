abf7683bfba468d6d6718553d02525262e64555c
ArcticYouko <arcticyouko@outlook.com>
2023-09-14 10:42:14 +0800

    Optimize Win32Handle.

--- Win32Handle.h
+++ Win32Handle.h
@@ -7,7 +7,7 @@ private:
 public:
     Win32Handle() noexcept;
     Win32Handle(void* handle) noexcept;
-    ~Win32Handle() noexcept;
+    virtual ~Win32Handle() noexcept;
 
     void* operator=(void* handle) noexcept;
     operator void*() const noexcept;
